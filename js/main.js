762e81aeacd1af3212f83f635c410b246fd6ac41
Eric Dahl <eric.c.dahl@gmail.com>
2014-01-12 14:01:16 -0600

    Update version of unjumble

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function() {
         $('form').submit(function(e) {
             var $results = $('#results');
             $results.empty();
-            _.forEach(unjumble(data, $('#query').val()), function(result) {
+            _.forEach(unjumble.solve(data, $('#query').val()), function(result) {
                 $results.append($('<ul>').html(result));
             });
             e.preventDefault();
