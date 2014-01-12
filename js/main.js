f97b06d6562964a3d84744eb0a4944417e7a3c5e
Eric Dahl <eric.c.dahl@gmail.com>
2014-01-11 20:49:15 -0600

    add button and form for submission

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 $(function() {
     $.getJSON('words.json', function(data) {
-        $('#query').keypress(function(e) {
+        $('form').submit(function(e) {
             var $results = $('#results');
             $results.empty();
-            if (e.which === 13) {
-                _.forEach(unjumble(data, $('#query').val()), function(result) {
-                    $results.append($('<ul>').html(result));
-                });
-            }
+            _.forEach(unjumble(data, $('#query').val()), function(result) {
+                $results.append($('<ul>').html(result));
+            });
+            e.preventDefault();
         });
     });
 });
