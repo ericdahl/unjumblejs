6286d6f18f7f9700a543ffbd359894c34c257580
Eric Dahl <eric.c.dahl@gmail.com>
2014-01-12 19:23:57 -0600

    add hints() function
    fixes #4

--- js/unjumble.js
+++ js/unjumble.js
@@ -1,36 +1,66 @@
 /* exported unjumble */
+
+// Revealing Module namespace pattern
 var unjumble = (function() {
     'use strict';
 
-    // public api
-    return {
-        solve: function(wordlist, word) {
-            var results = [];
+    var solve = function(wordlist, word) {
+        var results = [];
 
-            for (var i = 0, len = wordlist.length; i < len; ++i) {
+        for (var i = 0, len = wordlist.length; i < len; ++i) {
 
-                if (wordlist[i].length === word.length) {
-                    var candidateWord = wordlist[i],
-                        copy = word;
+            if (wordlist[i].length === word.length) {
+                var candidateWord = wordlist[i],
+                    copy = word;
 
-                    for (var j = 0; j < word.length; ++j) {
-                        var currentCharacter = candidateWord[j];
-                        var index = copy.indexOf(currentCharacter);
-                        if (index !== -1) {
-                            copy = copy.substring(0, index) + copy.substring(index + 1);
-                            if (copy.length === 0) {
-                                results.push(candidateWord);
-                                break;
-                            }
-                        } else {
+                for (var j = 0; j < word.length; ++j) {
+                    var currentCharacter = candidateWord[j];
+                    var index = copy.indexOf(currentCharacter);
+                    if (index !== -1) {
+                        copy = copy.substring(0, index) + copy.substring(index + 1);
+                        if (copy.length === 0) {
+                            results.push(candidateWord);
                             break;
                         }
+                    } else {
+                        break;
                     }
                 }
-
             }
-            return results;
         }
+        return results;
+    },
+    hints = function(wordlist, word) {
+        var answer,
+            currentHints;
+        return function() {
+            if (!answer) {
+                // only compute answer when needed (not on generation of function)
+                answer = solve(wordlist, word);
+
+                // initialize hints
+                (function() {
+                    currentHints = [];
+                    for (var i = 0, len = answer.length; i < len; ++i) {
+                        currentHints.push(new Array(answer[i].length + 1).join('_'));
+                    }
+                })();
+            }
+            // Loop through each hint, replacing the first '_' character with the correct letter
+            for (var i = 0, len = currentHints.length; i < len; ++i) {
+                var index = currentHints[i].indexOf('_');
+                if (index !== -1) {
+                    currentHints[i] = currentHints[i].replace(/_/, answer[i][index]);
+                }
+            }
+            return currentHints;
+        };
+    };
+
+    // public api
+    return {
+        solve: solve,
+        hints: hints
     };
 }());
 
--- tests/solve.js
+++ tests/solve.js
@@ -1,33 +1,57 @@
 var assert = require('assert'),
     unjumble = require('../js/unjumble.js');
 
-describe('unjumble', function () {
+describe('unjumble', function() {
     'use strict';
-    describe('#solve()', function () {
 
-        var words = [
-            'word',
-            'apple',
-            'dog',
-            'god'
-        ];
+    var words = [
+        'word',
+        'apple',
+        'dog',
+        'god'
+    ];
 
-        it('already matches', function () {
+    describe('#solve()', function() {
+        it('already matches', function() {
             assert.deepEqual(unjumble.solve(words, 'word'), ['word']);
         });
 
-        it('backwards', function () {
+        it('backwards', function() {
             assert.deepEqual(unjumble.solve(words, 'drow'), ['word']);
             assert.deepEqual(unjumble.solve(words, 'elppa'), ['apple']);
         });
 
-        it('no match', function () {
+        it('no match', function() {
             assert.deepEqual(unjumble.solve(words, 'zxya'), []);
         });
 
-        it('multiple matches', function () {
+        it('multiple matches', function() {
             assert.deepEqual(unjumble.solve(words, 'ogd'), ['dog', 'god']);
         });
+    });
 
+    describe('#hints()', function() {
+        it('generates basic hint', function() {
+            var hints = unjumble.hints(words, 'dwor');
+            assert.deepEqual(hints(), ['w___']);
+            assert.deepEqual(hints(), ['wo__']);
+            assert.deepEqual(hints(), ['wor_']);
+            assert.deepEqual(hints(), ['word']);
+            assert.deepEqual(hints(), ['word']);
+        });
+
+        it('handles no match', function() {
+            var hints = unjumble.hints(words, 'foobar');
+            assert.deepEqual(hints(), []);
+            assert.deepEqual(hints(), []);
+        });
+
+        it('handles multiple matches', function() {
+            var hints = unjumble.hints(words, 'dgo');
+            assert.deepEqual(hints(), ['d__', 'g__']);
+            assert.deepEqual(hints(), ['do_', 'go_']);
+            assert.deepEqual(hints(), ['dog', 'god']);
+            assert.deepEqual(hints(), ['dog', 'god']);
+        });
     });
 });
