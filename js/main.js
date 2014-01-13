038f3abc62eeb9e106f3472e5af101c24a2b446c
Eric Dahl <eric.c.dahl@gmail.com>
2014-01-12 20:45:50 -0600

    Add ability to get hints

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
 $(function() {
-    $.getJSON('words.json', function(data) {
-        $('form').submit(function(e) {
-            var $results = $('#results');
-            $results.empty();
-            _.forEach(unjumble.solve(data, $('#query').val()), function(result) {
-                $results.append($('<ul>').html(result));
-            });
-            e.preventDefault();
+    $.getJSON('words.json', function(words) {
+
+        $('#solve').click(function() {
+            app.solve(words);
+        });
+        $('#query').keypress(function(e) {
+            if (e.which === 13) {
+                app.solve(words);
+            }
+        });
+        $('#hint').click(function() {
+            app.hint(words);
         });
     });
+
 });
+
+var app = (function() {
+    var hints = {},
+        solve = function(words) {
+            var $results = $('#results');
+            $results.empty();
+            _.forEach(unjumble.solve(words, $('#query').val()), function(result) {
+                $results.append($('<ul>').html(result));
+            });
+        },
+        hint = function(words) {
+            var $query = $('#query');
+            var queryValue = $query.val(),
+                $results = $('#results');
+
+            if (!hints[queryValue]) {
+                hints[queryValue] = unjumble.hints(words, queryValue);
+            }
+            $results.empty().append(_.map(hints[queryValue](), function(result) {
+                return $('<ul>').html(result);
+            }));
+        };
+
+    return {
+        solve: solve,
+        hint: hint
+    }
+}());
