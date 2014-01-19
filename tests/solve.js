4096193bdab1f3c60d4a8b61b030884944427a0c
Eric Dahl <eric.c.dahl@gmail.com>
2014-01-18 20:22:18 -0600

    - Setup index.html for mocha to allow for running tests in a browser, rather than just nodejs. - Switch to chai assertion library rather than assert so that it works in the browser as well

--- tests/solve.js
+++ tests/solve.js
@@ -1,5 +1,10 @@
-var assert = require('assert'),
-    unjumble = require('../js/unjumble.js');
+if (typeof(require) !== 'undefined') {
+    var expect = require('chai').expect,
+        unjumble = require('../js/unjumble.js');
+} else {
+    /* global chai */
+    var expect = chai.expect;
+}
 
 describe('unjumble', function() {
     'use strict';
@@ -13,45 +18,45 @@ describe('unjumble', function() {
 
     describe('#solve()', function() {
         it('already matches', function() {
-            assert.deepEqual(unjumble.solve(words, 'word'), ['word']);
+            expect(unjumble.solve(words, 'word')).to.deep.equal(['word']);
         });
 
         it('backwards', function() {
-            assert.deepEqual(unjumble.solve(words, 'drow'), ['word']);
-            assert.deepEqual(unjumble.solve(words, 'elppa'), ['apple']);
+            expect(unjumble.solve(words, 'drow')).to.deep.equal(['word']);
+            expect(unjumble.solve(words, 'elppa')).to.deep.equal(['apple']);
         });
 
         it('no match', function() {
-            assert.deepEqual(unjumble.solve(words, 'zxya'), []);
+            expect(unjumble.solve(words, 'zxya')).to.have.length(0);
         });
 
         it('multiple matches', function() {
-            assert.deepEqual(unjumble.solve(words, 'ogd'), ['dog', 'god']);
+            expect(unjumble.solve(words, 'ogd')).to.deep.equal(['dog', 'god']);
         });
     });
 
     describe('#hints()', function() {
         it('generates basic hint', function() {
             var hints = unjumble.hints(words, 'dwor');
-            assert.deepEqual(hints(), ['w___']);
-            assert.deepEqual(hints(), ['wo__']);
-            assert.deepEqual(hints(), ['wor_']);
-            assert.deepEqual(hints(), ['word']);
-            assert.deepEqual(hints(), ['word']);
+            expect(hints()).to.deep.equal(['w___']);
+            expect(hints()).to.deep.equal(['wo__']);
+            expect(hints()).to.deep.equal(['wor_']);
+            expect(hints()).to.deep.equal(['word']);
+            expect(hints()).to.deep.equal(['word']);
         });
 
         it('handles no match', function() {
             var hints = unjumble.hints(words, 'foobar');
-            assert.deepEqual(hints(), []);
-            assert.deepEqual(hints(), []);
+            expect(hints()).to.have.length(0);
+            expect(hints()).to.have.length(0);
         });
 
         it('handles multiple matches', function() {
             var hints = unjumble.hints(words, 'dgo');
-            assert.deepEqual(hints(), ['d__', 'g__']);
-            assert.deepEqual(hints(), ['do_', 'go_']);
-            assert.deepEqual(hints(), ['dog', 'god']);
-            assert.deepEqual(hints(), ['dog', 'god']);
+            expect(hints()).to.deep.equal(['d__', 'g__']);
+            expect(hints()).to.deep.equal(['do_', 'go_']);
+            expect(hints()).to.deep.equal(['dog', 'god']);
+            expect(hints()).to.deep.equal(['dog', 'god']);
         });
     });
 });
